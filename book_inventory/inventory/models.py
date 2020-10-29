12f660ca66563fae780ca6603a5252c7213f33e5
Bharat Ahuja <bharatahuja198@gmail.com>
2020-10-29 18:54:34 +0530

    added model

---
+++ book_inventory/inventory/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length = 200, blank = False)
+    author = models.CharField(max_length = 200, blank = False)  
+    google_id = models.CharField(max_length = 50, blank = False) #Google Books Volume ID
+    stock = models.PositiveIntegerField()  #Number of Items In Inventory
+
+    choices = (
+        ('Available', 'Item is in stock'),
+        ('Out Of Stock', 'Item is out of stock'),
+        ('Not Available', 'Item is not in inventory')
+    )
+    
+    status = models.CharField(max_length = 15, choices = choices, blank = False)  #Available, Out Of Stock, Not Available
+
+    def __str__(self):
+        return 'Title: {0} Author: {1} Stock: {2}'.format(self.title, self.author, self.stock)
