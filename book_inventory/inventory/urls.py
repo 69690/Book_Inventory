dedd0bba9a9f09e0a66c2cb551472fc454940dea
Bharat Ahuja <bharatahuja198@gmail.com>
2020-10-30 12:44:52 +0530

    added validation checks and edit, delete functionality

--- book_inventory/inventory/urls.py
+++ book_inventory/inventory/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^display_books$',index, name='display_books'),
-    url(r'^add_book$',add_book, name='add_book')
+    url(r'^add_book$',add_book, name='add_book'),
+    url(r'^edit_book/(?P<pk>\d+)$',edit_book, name='edit_book'),
+    url(r'^delete_book/(?P<pk>\d+)$',delete_book, name='delete_book')
 ]
--- book_inventory/inventory/views.py
+++ book_inventory/inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
 
@@ -8,29 +8,43 @@ def index(request):
     items = Book.objects.all()
 
     context = {
-        'items': items,
+        'items': items, #table
     }
 
-    return render(request, 'index.html', context = context)
-
-def display_books(request):
-    items = Book.objects.all()
-
-    context = {
-        'items': items,
-    }
-
-    return render(request, 'index.html', context = context)
+    return render(request, 'index.html', context = context) #render table in main page
 
 def add_book(request):
     if request.method == "POST":
         form = BookForm(request.POST)
 
         if form.is_valid():
+            google_id = form.cleaned_data['google_id']   #Getting Google_ID field from the form which is automatically filled with AJAX API request
+            print(google_id)
             form.save()
             return redirect('index')
-        else:
-            print("WTF")
+
     else:
         form = BookForm()
         return render(request, 'add_book.html', {'form': form})
+
+def edit_book(request, pk):
+    item = get_object_or_404(Book, pk=pk) #select statement with pk
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=item)   #populate form with information with the information brought earlier
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = BookForm(instance=item)
+        return render(request, 'edit_book.html', {'form':form})
+
+def delete_book(request, pk):
+    Book.objects.filter(id=pk).delete()
+    
+    items = Book.objects.all()
+
+    context = {
+        'items': items
+    }
+
+    return render(request, 'index.html', context)
