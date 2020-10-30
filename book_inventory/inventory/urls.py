67310e3c3add81fde2c1a51427199798504fa31c
Bharat Ahuja <bharatahuja198@gmail.com>
2020-10-30 17:10:33 +0530

    Final version
    Fixed Bugs. Incorporated Google Books API and created search function along with visual appeal.

--- book_inventory/inventory/urls.py
+++ book_inventory/inventory/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from django.urls import include, path
 from .views import *
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^add_book$',add_book, name='add_book'),
     url(r'^edit_book/(?P<pk>\d+)$',edit_book, name='edit_book'),
-    url(r'^delete_book/(?P<pk>\d+)$',delete_book, name='delete_book')
+    url(r'^delete_book/(?P<pk>\d+)$',delete_book, name='delete_book'),
+    path('search_book',search_book, name='search_book'),
+    url(r'^about$',about,name='about')
 ]
--- book_inventory/inventory/views.py
+++ book_inventory/inventory/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
 from django.contrib import messages
+import requests
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -79,3 +81,97 @@ def delete_book(request, pk):
 
     messages.success(request, "Book Successfully Deleted")
     return render(request, 'index.html', context)
+
+def search_book(request):
+    #https://www.googleapis.com/books/v1/volumes
+    # ?q=flowers+inauthor:keyes&key=AIzaSyCeb83AnvleYBTR9XFIQVl82cNeQyw_q0c
+    try:
+        query = request.GET['query']
+        URL = 'https://www.googleapis.com/books/v1/volumes'
+        PARAMS = {
+            'q': query, 
+            'key': 'AIzaSyCeb83AnvleYBTR9XFIQVl82cNeQyw_q0c'
+        }
+
+        r = requests.get(url = URL, params = PARAMS)
+
+        class Book_s:
+            def __init__(self, title, author, google_id, description=''):
+                self.pk = None
+                self.title = title
+                self.author = author
+                self.google_id = google_id
+                self.description = description
+                if not Book.objects.filter(google_id = google_id).exists(): #If not in inventory
+                    self.status = "Not Available"
+                else: #if book is in inventory
+                    item = get_object_or_404(Book, google_id = google_id) #fetching item
+
+                    #checking if stock > 0
+                    if item.stock > 0:
+                        self.pk = item.pk
+                        self.status = "Available"
+                    else:
+                        self.pk = item.pk
+                        self.status = "Out Of Stock"
+
+        data = r.json()
+
+        #creating a list to store all objects from the search result along with their status in inventory using above class
+        search_results = []
+        for i in data['items']:
+            try: #check if all fields exist
+                #x = i['volumeInfo']['authors']
+                t = i['volumeInfo']['title']
+                a = i['volumeInfo']['authors']
+                st_a = ''
+                if len(a) == 1:
+                    st_a += a[0]
+                else:
+                    for i in range(len(a)-1):
+                        st_a += a[i]
+                        st_a += ', '
+                    st_a += a[-1]
+
+                i_d = i['id']
+                try:
+                    d = i['volumeInfo']['description']
+                    obj = Book_s(t,st_a,i_d,d)
+                except:
+                    obj = Book_s(t,st_a,i_d)
+                
+                # print(obj.google_id)
+                # print(obj.status)
+                # print()
+                search_results.append(obj)
+                # print(item['id'])
+                # print(item['volumeInfo']['authors'])
+                #print()
+            except:
+                print(i)
+                print()
+                pass
+
+                #print(i['volumeInfo'])
+        # for key, value in data['items'].items():
+        #     print(key,' : ',value)
+        
+        # for item in data['items']:
+        #     for v in item['volumeInfo']:
+        #         if 'authors' in v.keys():
+        #             print(v['authors'])
+            #print(item['volumeInfo']['authors'])
+        context = {
+            'search_results': search_results,
+            'length': len(search_results)
+        }
+
+        messages.success(request, 'Search Results...')
+        return render(request, 'search.html', context)
+    except:
+        messages.error(request, "Enter a search query")
+        return redirect('index')
+
+def about(request):
+    messages.info(request, "Welcome To About Me")
+    return render(request, 'about.html')
